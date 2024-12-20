63da4914c5fbe54067b8158668f3a9784b2214a7
BoppleOpple <boppleopple@gmail.com>
2024-12-20 18:48:30 -0500

    commented stringUtils

--- src/stringUtils.c
+++ src/stringUtils.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 char *slice(const char *s, int start, int end) {
+	// standard splitting function
 	char *substring = malloc(sizeof(char) * (end - start + 1));
 	
 	for (int i = start; i < end; i++)
@@ -15,41 +16,55 @@ char *slice(const char *s, int start, int end) {
 	return substring;
 }
 
+// splits a string everywhere a function returns truthy
 LIST splitAtFunction(const char *s, int (fn)(const char, void *args), void *args) {
+	// init list
 	LIST sliced = listCreate();
 
-	int lastSpace = -1;
+	// keep track of the beginning of the substring
+	int lastFalse = -1;
 	for (int i = 0; i < strlen(s); i++) {
+		// add a substring to the list if true
 		if (fn(*(s + i), args)) {
-			listAppendItem(&sliced, slice(s, lastSpace + 1, i));
-			lastSpace = i;
+			listAppendItem(&sliced, slice(s, lastFalse + 1, i));
+
+			// reset the beginning
+			lastFalse = i;
 		}
 	}
 
-	if (lastSpace != strlen(s) - 1)
-		listAppendItem(&sliced, slice(s, lastSpace + 1, strlen(s)));
+	// append the last bit from the last false location to the end
+	if (lastFalse != strlen(s) - 1)
+		listAppendItem(&sliced, slice(s, lastFalse + 1, strlen(s)));
 
 	return sliced;
 }
 
+// helper function that checks if two chars are equal
 int charEquality(const char a, void *b) {
 	return a == *(char *) b;
 }
 
 LIST split(const char *s, char character) {
+	// call splitAtFunction with the helper
 	return splitAtFunction(s, &charEquality, &character);
 }
 
+// helper function that checks if a character is a space
 int spaceComparison(const char a, void *_b) {
 	return isspace(a);
 }
 
 LIST splitSpaces(const char *s) {
+	// call splitAtFunction with the helper
 	LIST sliced = splitAtFunction(s, &spaceComparison, NULL);
+
+	// remove empties
 	for (int i = 0; i < sliced.size; i++) {
 		if (*(char *) listGetElement(&sliced, i) == 0) {
 			free(listPop(&sliced, i));
 		}
 	}
+
 	return sliced;
 }
--- src/stringUtils.h
+++ src/stringUtils.h
@@ -3,10 +3,31 @@
 #ifndef RAYTRACE_STRINGUTILS
 #define RAYTRACE_STRINGUTILS
 
+/**
+ * @brief creates a new string that contains characters from start to end-1, inclusive
+ *
+ * @param s char* string to slice
+ * @param start int first index to include
+ * @param end int first index to not be included
+ * @return char* substring
+ */
 char *slice(const char *s, int start, int end);
 
+/**
+ * @brief generates a list of substrings in between each instance of a specified character in a string
+ *
+ * @param s char* string to split
+ * @param character char character to split by
+ * @return LIST a list of all of the substrings between the specified characters
+ */
 LIST split(const char *s, char character);
 
+/**
+ * @brief generates a list of substrings in between each whitespace character in a string
+ *
+ * @param s char* string to split
+ * @return LIST a list of all of the substrings between whitespace
+ */
 LIST splitSpaces(const char *s);
 
 #endif
