9280aaf7e74be5890b06c06e290bfacaf6a46dfd
BoppleOpple <boppleopple@gmail.com>
2024-12-20 21:46:56 -0500

    commented matrix files

--- src/matrix.c
+++ src/matrix.c
@@ -9,20 +9,27 @@
 #define DOUBLE_STRING_LENGTH 3 + DBL_MANT_DIG - DBL_MIN_EXP
 
 MATRIX createTempMatrix(int r, int c) {
+	// create a new matrix
 	MATRIX m = {r, c, malloc( sizeof(double) * r * c )};
+	// fill its data with zeros
 	fillMatrix(&m, 0.0);
 	return m;
 }
 
 MATRIX *createMatrix(int r, int c) {
+	// create a new matrix of zeros
 	MATRIX *m = malloc(sizeof(MATRIX));
 	*m = createTempMatrix(r, c);
-	fillMatrix(m, 0.0);
+
+	// and return a pointer to it
 	return m;
 }
 
 MATRIX *createIdentityMatrix(int s) {
+	// create a zero matrix
 	MATRIX *m = createMatrix(s, s);
+
+	// and set the diagonal to ones
 	for (int i = 0; i < s; i++)
 		setElement(m, i, i, 1.0);
 	
@@ -30,8 +37,10 @@ MATRIX *createIdentityMatrix(int s) {
 }
 
 MATRIX *copyMatrix(MATRIX *m) {
+	// create a new matrix
 	MATRIX *copy = createMatrix(m->rows, m->cols);
 
+	// set each element to the same element in m
 	for (int i = 0; i < m->rows * m->cols; i++)
 		*(copy->data + i) = *(m->data + i);
 
@@ -39,102 +48,112 @@ MATRIX *copyMatrix(MATRIX *m) {
 }
 
 MATRIX *createVector(double x, double y, double z) {
+	// create a new matrix
 	MATRIX *m = malloc(sizeof(MATRIX));
 	
+	// set the coordinates (plus a homogenous coordinate)
 	*m = (MATRIX) {4, 1, malloc( sizeof(double) * 4 )};
 	*(m->data + 0) = x;
 	*(m->data + 1) = y;
 	*(m->data + 2) = z;
 	*(m->data + 3) = 1.0;
+
 	return m;
 }
 
 void addScalar(MATRIX *m, double s) {
+	// iterate through each element and add s
 	for (int i = 0; i < m->rows * m->cols; i++)
 		*(m->data + i) += s;
 }
 
+// TODO do this in-place
 void addMatrix(MATRIX *m1, MATRIX *m2) {
+	// check the operation is valid
 	if (m1->rows != m2->rows || m1->cols != m2->cols) {
 		printf("matrices must be the same size; given (%i, %i) and (%i, %i)\n", m1->rows, m1->cols, m2->rows, m2->cols);
 		exit(1);
 	}
-
-	MATRIX result = createTempMatrix(m1->rows, m1->cols);
-	for (int i = 0; i < result.rows * result.cols; i++)
-		*(result.data + i) = *(m1->data + i) + *(m2->data + i);
-	
-	freeMatrix(m1);
-
-	*m1 = result;
+	// increment each value accordingly
+	for (int i = 0; i < m1->rows * m1->cols; i++)
+		*(m1->data + i) += *(m2->data + i);
 }
 
 void multScalar(MATRIX *m, double s) {
+	// iterate through each element and multiply by s
 	for (int i = 0; i < m->rows * m->cols; i++)
 		*(m->data + i) *= s;
 }
 
 void multMatrixTo(MATRIX *m1, MATRIX *m2, MATRIX **dst) {
+	// check the operation is valid
 	if (m1->cols != m2->rows) {
 		printf("matrices must be size m x n and n x p; given (%i, %i) and (%i, %i)\n", m1->rows, m1->cols, m2->rows, m2->cols);
 		exit(1);
 	}
 
+	// create a temp matrix
 	MATRIX m = createTempMatrix(m1->rows, m2->cols);
 
+	// perform the matrix multiplication
 	for (int i = 0; i < m.rows; i++)
 		for (int j = 0; j < m.cols; j++)
 			for (int k = 0; k < m1->cols; k++)
 				setElement(&m, i, j, getElement(&m, i, j) + getElement(m1, i, k)*getElement(m2, k, j) );
 	
+	// setup the memory at *dst
 	if (*dst) {
-		// printf("dst exists ig: %p\n", dst);
 		freeMatrix(*dst);
 	} else {
 		*dst = malloc(sizeof(MATRIX));
-		// printf("allocating (%i, %i) matrix\n", m.rows, m.cols);
 	}
+
+	// store the result
 	**dst = m;
 }
 
 void multMatrix(MATRIX *m1, MATRIX *m2) {
+	// just multiply with destination m1
 	multMatrixTo(m1, m2, &m1);
 }
 
 void multMatrixElementwise(MATRIX *m1, MATRIX *m2) {
+	// check the operation is valid
 	if (m1->rows != m2->rows || m1->cols != m2->cols) {
 		printf("matrices must be the same size; given (%i, %i) and (%i, %i)\n", m1->rows, m1->cols, m2->rows, m2->cols);
 		exit(1);
 	}
 
-	MATRIX m = createTempMatrix(m1->rows, m1->cols);
-	for (int i = 0; i < m.rows * m.cols; i++)
-		*(m.data + i) = *(m1->data + i) * *(m2->data + i);
-
-	freeMatrix(m1);
-	
-	*m1 = m;
+	// multiply each element
+	for (int i = 0; i < m1->rows * m1->cols; i++)
+		*(m1->data + i) *= *(m2->data + i);
 }
 
 void fillMatrix(MATRIX *m, double n) {
+	// set each element in m to n
 	for (int i = 0; i < m->rows * m->cols; i++)
 		*(m->data + i) = n;
 }
 
 double getElement(MATRIX *m, int r, int c) {
+	// just a wrapper for pointer math
 	return *(m->data + m->cols * r + c);
 }
 
 void setElement(MATRIX *m, int r, int c, double val) {
+	// still just a wrapper for pointer math
 	*(m->data + m->cols * r + c) = val;
 }
 
 double minor(MATRIX *m, int r, int c) {
 	MATRIX *submatrix = createMatrix(m->rows-1, m->cols-1);
+
+	// create a submatrix by removing row r and column c
 	for (int i = 0; i < submatrix->rows; i++)
 		for (int j = 0; j < submatrix->cols; j++)
 			setElement(submatrix, i, j, getElement(m, (i<r) ? i : i+1, (j<c) ? j : j+1));
 	
+	// the minor is the determinant of the matrix
 	double result = determinant(submatrix);
 
 	freeMatrix(submatrix);
@@ -144,19 +163,23 @@ double minor(MATRIX *m, int r, int c) {
 }
 
 double cofactor(MATRIX *m, int r, int c) {
+	// positive or negative monir of m, depending on r and c
 	return (((r + c) % 2) ? -1 : 1) * minor(m, r, c);
 }
 
 double determinant(MATRIX *m) {
+	// check the operation is valid
 	if (m->rows != m->cols) {
 		printf("cannot find the determinant of non-square matrix (%ix%i)", m->rows, m->cols);
 		exit(1);
 	}
+
+	// base case: determinant of 1x1 matrix is its only value
 	if (m->rows == 1)
 		return getElement(m, 0, 0);
 
+	// for everything else, det is the sum of all cofactors scaled by their elements
 	double result = 0.0;
-
 	for (int i = 0; i < m->cols; i++)
 		result += getElement(m, 0, i) * cofactor(m, 0, i);
 	
@@ -164,24 +187,26 @@ double determinant(MATRIX *m) {
 }
 
 MATRIX *getInverse(MATRIX *m) {
+	// setup temp variables
 	double det = determinant(m);
 	MATRIX *result = createMatrix(m->rows, m->cols);
 
+	// check the operation is valid
 	if (det == 0) {
 		printf("matrix is not invertible!\n");
 		exit(1);
 	}
 
+	// each element of the inverse is the cofactor of that element in m / |m|
 	for (int i = 0; i < result->rows; i++)
 		for (int j = 0; j < result->cols; j++)
-			setElement(result, i, j, cofactor(m, j, i));
-
-	multScalar(result, 1.0/det);
+			setElement(result, i, j, cofactor(m, j, i) / det);
 
 	return result;
 }
 
 void rref(MATRIX *m) {
+	// check the operation is valid
 	if (m->cols < m->rows) {
 		printf("matrix is underconstrained\n");
 		exit(1);
@@ -193,6 +218,7 @@ void rref(MATRIX *m) {
 		exit(1);
 	}
 
+	// first, just row reduce by making each value under the diagonal 0
 	for (int i = 0; i < m->rows; i++) {
 		if (getElement(m, i, i) == 0) {
 			for (int j = i + 1; j < m->rows; j++) {
@@ -202,13 +228,12 @@ void rref(MATRIX *m) {
 				}
 			}
 		}
-		
-		
 		scaleRow(m, i, 1.0/getElement(m, i, i));
 		for (int j = i + 1; j < m->rows; j++)
 			addRowMultiple(m, i, j, -getElement(m, j, i));
 	}
 
+	// then, do rescaling and such so that every value above the diagonal is alzo 0 (except the rightmost column)
 	for (int i = m->rows - 1; i >= 0; i--)
 		for (int j = i - 1; j >= 0; j--)
 			addRowMultiple(m, i, j, -getElement(m, j, i));
@@ -216,18 +241,23 @@ void rref(MATRIX *m) {
 }
 
 void scaleRow(MATRIX *m, int r, double scale) {
+	// just rescale each element in a row
 	for (int j = 0; j < m->cols; j++)
 		setElement(m, r, j, scale * getElement(m, r, j));
 }
 
 void addRowMultiple(MATRIX *m, int sourceRow, int destinationRow, double scale) {
+	// just increment each element in a row by some multiple of another row
 	for (int j = 0; j < m->cols; j++)
 		setElement(m, destinationRow, j, getElement(m, destinationRow, j) + scale * getElement(m, sourceRow, j));
 }
 
 void swapRows(MATRIX *m, int r1, int r2) {
 	double temp = 0.0;
+
+	// for each element in both rows
 	for (int j = 0; j < m->cols; j++) {
+		// swap the corresponding elements
 		temp = getElement(m, r1, j);
 		setElement(m, r1, j, getElement(m, r2, j));
 		setElement(m, r2, j, temp);
@@ -236,6 +266,8 @@ void swapRows(MATRIX *m, int r1, int r2) {
 
 double vectorDotProduct(MATRIX *v1, MATRIX *v2) {
 	double result = 0.0;
+
+	// make result the sum of element-wise multiplication
 	for (int i = 0; i < v1->rows - 1; i++)
 		result += getElement(v1, i, 0)*getElement(v2, i, 0);
 	
@@ -243,6 +275,7 @@ double vectorDotProduct(MATRIX *v1, MATRIX *v2) {
 }
 
 MATRIX *vectorCrossProduct(MATRIX *v1, MATRIX *v2) {
+	// this is just the definition of a cross product
 	return createVector(
 		getElement(v1, 1, 0)*getElement(v2, 2, 0) - getElement(v1, 2, 0)*getElement(v2, 1, 0),
 		getElement(v1, 2, 0)*getElement(v2, 0, 0) - getElement(v1, 0, 0)*getElement(v2, 2, 0),
@@ -251,6 +284,7 @@ MATRIX *vectorCrossProduct(MATRIX *v1, MATRIX *v2) {
 }
 
 double vectorLength(MATRIX *v) {
+	// return L2 norm of a vector
 	return sqrt(
 		pow(getElement(v, 0, 0), 2.0) +
 		pow(getElement(v, 1, 0), 2.0) +
@@ -259,13 +293,16 @@ double vectorLength(MATRIX *v) {
 }
 
 void normalizeVector(MATRIX *v) {
+	// rescale a vector by the inverse of its length
 	multScalar(v, 1.0/vectorLength(v));
 }
 
 int isZeroMatrix(MATRIX *m) {
+	// for each element
 	for (int i = 0; i < m->rows; i++)
 		for (int j = 0; j < m->cols; j++)
-			if (getElement(m, i, j) != 0.0) return 1;
+			// return falsey if it isnt 0
+			if (getElement(m, i, j) != 0.0) return 0;
 	
 	return 1;
 }
@@ -274,14 +311,17 @@ void printMatrix(MATRIX *m) {
 	int max_digits = 0;
 	char digitString[DOUBLE_STRING_LENGTH] = "";
 
+	// find the largest number printed and set the length of all elements to that
 	for (int i = 0; i < m->rows * m->cols; i++) {
 		sprintf(digitString, "%lf", *(m->data + i));
 		max_digits = MAX(max_digits, strlen(digitString));
 	}
 
+	// now print each row
 	for (int i = 0; i < m->rows; i++) {
 		printf((i == 0) ? "[ " : "| ");
 		for (int j = 0; j < m->cols; j++) {
+			// and for each element insure it is the length of the longest element
 			printf("%*lf ", max_digits, getElement(m, i, j));
 		}
 		printf("%c\n", (i == m->rows - 1) ? ']' : '|');
@@ -289,10 +329,12 @@ void printMatrix(MATRIX *m) {
 }
 
 void printVector3(MATRIX *m) {
+	// just print the values of a vector in angle brackets
 	printf("<%lf, %lf, %lf>\n", m->data[0], m->data[1], m->data[2]);
 }
 
 void freeMatrix(MATRIX *m) {
+	// free the data and reset other information
 	free(m->data);
 	m->rows = 0;
 	m->cols = 0;
--- src/matrix.h
+++ src/matrix.h
@@ -8,82 +8,243 @@ typedef struct {
 } MATRIX;
 
 /**
- * @brief Create a Matrix struct
+ * @brief create a new MATRIX struct
  *
- * Creates a new matrix and fills it with zeros 
+ * creates a new matrix and fills it with zeros 
  *
- * @param r Number of rows
- * @param c Number of columns
+ * @param r int number of rows
+ * @param c int number of columns
  * @return MATRIX New zero matrix
  */
 MATRIX *createMatrix(int r, int c);
 
 /**
- * @brief Create a square identity matrix
+ * @brief create a square identity matrix
  * 
- * @param s dimensions of the square matrix (i.e. 4 -> 4x4 matrix)
- * @return MATRIX 
+ * @param s int dimensions of the square matrix (i.e. 4 -> 4x4 matrix)
+ * @return MATRIX the new identity matrix
  */
 MATRIX *createIdentityMatrix(int s);
 
+/**
+ * @brief copies the data of a MATRIX to a new MATRIX and returns it
+ *
+ * @param m MATRIX the source matrix
+ * @return MATRIX the returned copy
+ */
 MATRIX *copyMatrix(MATRIX *m);
 
 /**
- * @brief Create a Vector object
+ * @brief create a column vactor MATRIX struct
  * 
- * @param x 
- * @param y 
- * @param z 
+ * @param x double the vector's x component
+ * @param y double the vector's y component
+ * @param z double the vector's z component
  * @return MATRIX 
  */
 MATRIX *createVector(double x, double y, double z);
 
+/**
+ * @brief adds a scalar to each element of a MATRIX
+ *
+ * @param m MATRIX the matrix to increment
+ * @param s double the scalar to add
+ */
 void addScalar(MATRIX *m, double s);
 
+/**
+ * @brief adds a MATRIX to another MATRIX element-wise
+ *
+ * @param m1 MATRIX the matrix to increment
+ * @param m2 MATRIX the matrix to increment by
+ */
 void addMatrix(MATRIX *m1, MATRIX *m2);
 
+/**
+ * @brief multiplies each element of a MATRIX by a scalar
+ *
+ * @param m MATRIX the matrix to rescale
+ * @param s double the scalar to multiply by
+ */
 void multScalar(MATRIX *m, double s);
 
+/**
+ * @brief performs matrix multiplication on two MATRIX structs and stores the result in another MATRIX
+ *
+ * @param m1 MATRIX the left matrix
+ * @param m2 MATRIX the right matrix
+ * @param m2 MATRIX the destination matrix
+ */
 void multMatrixTo(MATRIX *m1, MATRIX *m2, MATRIX **dst);
 
+/**
+ * @brief performs matrix multiplication on two MATRIX structs and stores the result in the first MATRIX
+ *
+ * @param m1 MATRIX the left matrix (and the destination)
+ * @param m2 MATRIX the right matrix
+ */
 void multMatrix(MATRIX *m1, MATRIX *m2);
 
+/**
+ * @brief multiplies a MATRIX by another MATRIX element-wise
+ *
+ * @param m1 MATRIX the matrix to multiply
+ * @param m2 MATRIX the matrix to multiply by
+ */
 void multMatrixElementwise(MATRIX *m1, MATRIX *m2);
 
+/**
+ * @brief sets each element of a MATRIX to a value
+ *
+ * @param m MATRIX the matrix to modify
+ * @param n double the value to set
+ */
 void fillMatrix(MATRIX *m, double n);
 
+/**
+ * @brief gets the value of a specific element of a MATRIX
+ *
+ * @param m MATRIX the matrix to read from
+ * @param r int the row of the element
+ * @param c int the column of the element
+ * @return double the value of the element in row r, column c
+ */
 double getElement(MATRIX *m, int r, int c);
 
+/**
+ * @brief sets the value of a specific element of a MATRIX to a specified value
+ *
+ * @param m MATRIX the matrix to read from
+ * @param r int the row of the element
+ * @param c int the column of the element
+ * @param val double the value to store in row r, column c
+ */
 void setElement(MATRIX *m, int r, int c, double val);
 
+/**
+ * @brief computes and returns the minor of an element of a matrix
+ *
+ * @param m MATRIX the matrix to read from
+ * @param r int the row of the element
+ * @param c int the column of the element
+ * @return double the value of the minor
+ */
 double minor(MATRIX *m, int r, int c);
 
+/**
+ * @brief computes and returns the cofactor of an element of a matrix
+ *
+ * @param m MATRIX the matrix to read from
+ * @param r int the row of the element
+ * @param c int the column of the element
+ * @return double the value of the cofactor
+ */
 double cofactor(MATRIX *m, int r, int c);
 
+/**
+ * @brief computes and returns the determinant of a matrix
+ *
+ * @param m MATRIX the matrix to read from
+ * @return double the value of the determinant
+ */
 double determinant(MATRIX *m);
 
+/**
+ * @brief computes and returns the inverse of a matrix
+ *
+ * @param m MATRIX the matrix to read from
+ * @return MATRIX the matrix's inverse
+ */
 MATRIX *getInverse(MATRIX *m);
 
+/**
+ * @brief row reduces a matrix in-place
+ *
+ * @param m MATRIX the matrix to reduce
+ */
 void rref(MATRIX *m);
 
+/**
+ * @brief scales a row of a MATRIX by a specified factor in-place
+ *
+ * @param m MATRIX the matrix to operate on
+ * @param r MATRIX the row to scale
+ * @param scale double the value to scale by
+ */
 void scaleRow(MATRIX *m, int r, double scale);
 
+/**
+ * @brief increments one row of a matrix by a multiple of another row of a matrix in-place
+ *
+ * @param m MATRIX the matrix to operate on
+ * @param sourceRow int the row to increment by
+ * @param destinationRow int the row to add to
+ * @param scale double the value to scale by
+ */
 void addRowMultiple(MATRIX *m, int sourceRow, int destinationRow, double scale);
 
+/**
+ * @brief swaps two rows of a matrix in-place
+ *
+ * @param m MATRIX the matrix to operate on
+ * @param r1 int the first row to swap
+ * @param r2 int the second row to swap
+ */
 void swapRows(MATRIX *m, int r1, int r2);
 
+/**
+ * @brief computes and returns the dot product of two vectors
+ *
+ * @param v1 MATRIX the first operand vector
+ * @param v2 MATRIX the second operand vector
+ * @return double the value of the dot product
+ */
 double vectorDotProduct(MATRIX *v1, MATRIX *v2);
 
+/**
+ * @brief computes and returns the cross product of two vectors
+ *
+ * @param v1 MATRIX the first operand vector
+ * @param v2 MATRIX the second operand vector
+ * @return MATRIX the value of the cross product
+ */
 MATRIX *vectorCrossProduct(MATRIX *v1, MATRIX *v2);
 
+/**
+ * @brief computes and returns the norm of a vector
+ *
+ * @param v MATRIX the vector to read from
+ * @return double the vector's length
+ */
 double vectorLength(MATRIX *v);
 
+/**
+ * @brief rescales a vector so that it has length 1 in-place
+ *
+ * @param v MATRIX the vector to normalize
+ */
 void normalizeVector(MATRIX *v);
 
+/**
+ * @brief returns truthy if every element of a matrix is 0
+ *
+ * @param m MATRIX the matrix to read from
+ * @return int the boolean result
+ */
 int isZeroMatrix(MATRIX *m);
 
+/**
+ * @brief pretty prints a MATRIX struct
+ *
+ * @param m MATRIX the matrix to read from
+ */
 void printMatrix(MATRIX *m);
 
+/**
+ * @brief pretty prints a MATRIX struct that is a column vector
+ *
+ * @param m MATRIX the matrix to read from
+ */
 void printVector3(MATRIX *m);
 
 /**
