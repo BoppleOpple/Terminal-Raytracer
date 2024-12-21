e050d2b7ac6139885e951e058f3b036419c0979a
BoppleOpple <boppleopple@gmail.com>
2024-12-20 19:54:09 -0500

    commented debug files

--- src/debug.c
+++ src/debug.c
@@ -1,9 +1,11 @@
 #include "debug.h"
 
 unsigned int delta_us(struct timespec start, struct timespec end) {
+	// just do math
 	return (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
 }
 
 float delta_s(struct timespec start, struct timespec end) {
+	// and rescale the previous math
 	return (float) delta_us(start, end) / 1000000.0;
 }
--- src/debug.h
+++ src/debug.h
@@ -3,8 +3,22 @@
 #ifndef RAYTRACE_DEBUG
 #define RAYTRACE_DEBUG
 
+/**
+ * @brief returns the number of microseconds between two timespec structs
+ *
+ * @param start struct timespec the start timespec struct
+ * @param end struct timespec the end timespec struct
+ * @return uint the delta in microseconds
+ */
 unsigned int delta_us(struct timespec start, struct timespec end);
 
+/**
+ * @brief returns the time in seconds between two timespec structs
+ *
+ * @param start struct timespec the start timespec struct
+ * @param end struct timespec the end timespec struct
+ * @return float the delta in seconds
+ */
 float delta_s(struct timespec start, struct timespec end);
 
 #endif
