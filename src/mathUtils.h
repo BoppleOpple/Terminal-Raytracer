6f9f393a8cb63e7a5c4cd261ca483d12aa81e02e
BoppleOpple <boppleopple@gmail.com>
2024-12-20 19:54:29 -0500

    commented math helper header

--- src/mathUtils.h
+++ src/mathUtils.h
@@ -1,6 +1,9 @@
 #ifndef RAYTRACE_MATHUTILS
 #define RAYTRACE_MATHUTILS
+
+// add defs for constants and simple macros
 #define MAX(a, b) (a > b) ? a : b;
 #define MIN(a, b) (a < b) ? a : b;
 #define PI 3.1415926535
+
 #endif
